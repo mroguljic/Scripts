4b3d7696cfb37ef3212c7854dc5f20271981ebcd
Matej Roguljic <matej.roguljic@gmail.com>
2018-03-23 15:19:28 +0100

    Add files via upload

--- stepLengths.py
+++ stepLengths.py
@@ -8,9 +8,9 @@ print nEvents, 'number of steps'
 
 correctedFile = r.TFile('Corrected.root','RECREATE')
 correctedTree = r.TTree("name","name")
-updatedZ		  = np.zeros(1,dtype=float)
-updatedX		  = np.zeros(1,dtype=float)
-updatedY		  = np.zeros(1,dtype=float)
+updatedZ      = np.zeros(1,dtype=float)
+updatedX      = np.zeros(1,dtype=float)
+updatedY      = np.zeros(1,dtype=float)
 updatedEDep   = np.zeros(1,dtype=float)
 branchZ       = correctedTree.Branch('z',updatedZ,'z/D')
 branchZ       = correctedTree.Branch('x',updatedX,'x/D')
@@ -18,35 +18,35 @@ branchZ       = correctedTree.Branch('y',updatedY,'y/D')
 branchZ       = correctedTree.Branch('eDep',updatedEDep,'eDep/D')
 
 for i,event in enumerate(tree):
-	if(i%10000==0):
-		print i
+  if(i%10000==0):
+    print i
 
-	z 	 = event.z
-	x 	 = event.x
-	y 	 = event.y
-	preZ = event.preZ
-	preX = event.preX
-	preY = event.preY
-	eDep = event.eDep
+  z    = event.z
+  x    = event.x
+  y    = event.y
+  preZ = event.preZ
+  preX = event.preX
+  preY = event.preY
+  eDep = event.eDep
 
-	postPos			= np.array([x,y,z])
-	prePos			= np.array([preX,preY,preZ])
-	track       = np.subtract(postPos,prePos)
-	trackLen    = np.linalg.norm(track)
-	direction   = track/trackLen
+  postPos     = np.array([x,y,z])
+  prePos      = np.array([preX,preY,preZ])
+  track       = np.subtract(postPos,prePos)
+  trackLen    = np.linalg.norm(track)
+  direction   = track/trackLen
 
-	if(prePos[2]<=0.):
-		tempPos = prePos
+  if(prePos[2]<=0.):
+    tempPos = prePos
 
-	correctedStep  = np.add(np.random.exponential(trackLen)*direction,tempPos)
-	updatedZ[0]		 = correctedStep[2]
-	updatedX[0]		 = correctedStep[0]
-	updatedY[0]		 = correctedStep[1]
-	updatedEDep[0] = eDep
+  correctedStep  = np.add(np.random.exponential(trackLen)*direction,tempPos)
+  updatedZ[0]    = correctedStep[2]
+  updatedX[0]    = correctedStep[0]
+  updatedY[0]    = correctedStep[1]
+  updatedEDep[0] = eDep
 
-	tempPos = correctedStep
+  tempPos = correctedStep
 
-	correctedTree.Fill()
+  correctedTree.Fill()
 
 correctedFile.Write()
 correctedFile.Close()
